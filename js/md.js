6eaab56d2c42a686b860576df6a7c5cb8d7db4f5
districtvisa <85084643+districtvisa@users.noreply.github.com>
2021-05-30 22:39:38 -0400

    Update md.js

--- js/md.js
+++ js/md.js
@@ -27,6 +27,7 @@ function appendElement (container, lines) {
   elInfo.classes.forEach((cls) => el.classList.add(cls));
   container.appendChild(el);
   lines.splice(0, 1);
+  console.log(lines);
   if (!lines.length) return;
   while (lines[0].startsWith("  ")) {
     if (lines[0].startsWith("    ")) {
