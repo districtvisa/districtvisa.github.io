d36fcc661b7cb4574017c0cbfcc3e31ab133b371
Joseph Swetnam <joe@fofx.co>
2021-06-01 23:53:21 -0400

    _

--- js/page.js
+++ js/page.js
@@ -1,8 +1,26 @@
 build_site({
-  page_type: "main",
   company_name: "District Visa",
-  title_bar_style: "light cursive",
-  cover_images: ["img/capitol.jpg"],
-  tagline: "Apostille and Document Authentication Services",
-  tagline_style: "light cursive"
+  title_bar_style: "light",
+  tagline_style: "light cursive",
+  site_title: "District Visa",
+  pages: {
+    "index.html": {
+      title: "Home",
+      cover_images: ["img/capitol.jpg"],
+      tagline: "Apostille and Document Authentication Services",
+      content: [
+        {
+          type: "action_tiles",
+          items: "pages"
+        }
+      ]
+    },
+    "contact.html": {
+      title: "Contact Us",
+      content: [{type: "paragraph", content: "Hello"}]
+    },
+    "order-form.html": {
+      title: "Order Form"
+    }
+  }
 });
--- js/site.js
+++ js/site.js
@@ -7,7 +7,8 @@ function create_element
   content
 })
 {
-  var el = document.createElement(tag_name);
+  tag_name = tag_name || "div";
+  let el = document.createElement(tag_name);
   if (class_name) el.className = class_name;
   if (content) el.innerHTML = content;
   if (attrs) {
@@ -19,58 +20,115 @@ function create_element
   return el;
 }
 
-function build_site
-({
-  page_type,
-  company_name,
-  title_bar_style,
-  cover_images,
-  tagline,
-  tagline_style
-})
-{
-  var main = document.getElementById("main");
+function add_action_tiles (parent, item, config) {
+  let {items} = item,
+      all_pages = items === "pages",
+      outer_div = create_element({
+        parent: parent,
+        class_name: "action-tiles"
+      }),
+      inner_div = create_element({parent: outer_div});
+  if (all_pages) items = Object.getOwnPropertyNames(config.pages);
+  items.forEach(function (item_config) {
+    let tile = {parent: inner_div, tag_name: "a", attrs: {}};
+    if (all_pages) {
+      if (config.path === item_config) return;
+      tile.attrs.href = item_config;
+      tile.content = config.pages[item_config].title;
+    }
+    create_element(tile);
+  });
+}
+
+function add_paragraph (parent, item, config) {
+  let {content} = item,
+      p = create_element({
+        parent: parent,
+        tag_name: "p",
+        content: content
+      });
+}
+
+let add_content = {
+  action_tiles: add_action_tiles,
+  paragraph: add_paragraph
+};
+
+function build_page (config) {
+  let {
+        site_title,
+        title,
+        root,
+        cover_images,
+        company_name,
+        title_bar_style,
+        tagline,
+        tagline_style,
+        content
+      } = config,
+      main = document.getElementById("main");
+  create_element({
+    parent: document.head,
+    tag_name: "title",
+    content: site_title + " | " + title
+  });
   main.classList.add("container");
-  var title_bar = create_element({
+  let title_bar = create_element({
         parent: main,
-        tag_name: "div",
         class_name: "title-bar title-bar-" + title_bar_style
       }),
-      brand_div = create_element({
+      brand = create_element({
         parent: title_bar,
-        tag_name: "div",
+        tag_name: "a",
+        attrs: {href: "index.html"},
         class_name: "brand",
         content: company_name
       });
-      cover_img_div = create_element({
-        parent: main,
-        tag_name: "div",
-        class_name: "cover-img bg-gray"
-      }),
-      cover_img = create_element({
-        parent: cover_img_div,
-        tag_name: "img",
-        attrs: {src: cover_images[0]}
-      });
-  if (tagline) {
-    var tagline_div = create_element({
+  if (cover_images) {
+    title_bar.classList.add("title-bar-fixed");
+    let cover_img_div = create_element({
           parent: main,
-          tag_name: "div",
+          class_name: "cover-img bg-gray"
+        }),
+        cover_img = create_element({
+          parent: cover_img_div,
+          tag_name: "img",
+          attrs: {src: cover_images[0]}
+        });
+  } else {
+    title_bar.classList.add("title-bar-sticky");
+  }
+  if (tagline) {
+    let tagline_div = create_element({
+          parent: main,
           class_name: "tagline tagline-" + tagline_style
         }),
         tagline_frame = {
           parent: tagline_div,
-          tag_name: "div",
           class_name: "tagline-frame"
         };
     create_element(tagline_frame);
-    create_element({parent: tagline_div, tag_name: "div", content: tagline});
+    create_element({parent: tagline_div, content: tagline});
     create_element(tagline_frame);
   }
-  var footer = create_element({
+  content.forEach((item) => add_content[item.type](main, item, config));
+  let footer = create_element({
         parent: main,
-        tag_name: "div",
         class_name: "footer",
         content: "&copy; " + company_name
       });
 }
+
+function build_site (config) {
+  let page;
+  Object.getOwnPropertyNames(config.pages).find(function (path) {
+    if (location.pathname.endsWith(path)) {
+      let this_page = config.pages[path];
+      page = Object.assign(this_page, config);
+      page.path = path;
+      return true;
+    }
+  });
+  build_page(page);
+}
+
